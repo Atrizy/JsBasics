f582c3a1c80afa9306cc941a44b4d85ee6ac7525
Atrizy <evankeeping21@gmail.com>
2021-11-03 00:33:29 -0230

    fixed my stupidity

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var userAge = 17;
 var is_subscribed = true;
 
 if (userAge >= 18 && is_subscribed === true){
-    console.log("The user is 18 and subscribed!")
+    console.log("The user is 18 and is subscribed!")
 } else if(userAge >=18 && is_subscribed === false) {
-    console.log("This is under 18 and not subscribed")
+    console.log("This user is 18 and is not subscribed")
 } else if(userAge <=17 && is_subscribed === true) {
     console.log("This user is under 18 and is subscribed")
 } else if(userAge <=17 && is_subscribed === false) {
     console.log("This user is under 18 and is not subscribed")
+} else {
+    console.log("Something went wrong!")
 }
